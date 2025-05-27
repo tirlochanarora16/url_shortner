451d7940087f1e9352fb007306d3ed4bc49a4321
tirlochan016 <trilochan@crashnova.com>
2025-05-27 23:27:31 +0530

    minor update

--- routes/urls.go
+++ routes/urls.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/tirlochanarora16/url_shortner/models"
@@ -9,20 +10,19 @@ import (
 
 func createShortUrl(c *gin.Context) {
 	// body with receive original_url
-	url, err := models.CheckUrlExists("https://example.com")
+	url, err := models.CheckUrlExists("https://examples.com")
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// no result found for the given URL
-	if url == nil {
-		c.JSON(404, gin.H{"message": "No result found"})
+	// URL already exists in the DB
+	if url != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Short URL for this URL already exists"})
 		return
 	}
 
-	fmt.Println(url.ID)
-
+	// create New record in the DB
 	c.JSON(200, gin.H{"url": url})
 }

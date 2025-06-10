b4af6504effa06c934685209e080117e688576ce
tirlochan016 <trilochan@crashnova.com>
2025-06-10 20:42:37 +0530

    update

--- routes/routes.go
+++ routes/routes.go
@@ -3,6 +3,9 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(server *gin.Engine) {
+	server.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "running successfully!"})
+	})
 	server.POST("/shorten", createShortUrl)
-	server.GET("/*shortCode", catchAllRoutes)
+	// server.GET("/*shortCode", catchAllRoutes)
 }
